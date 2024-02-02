ac1490f53433b16fdd7a01796505f7cd44bafa6e
Yasir566 <skshaheen506@gmail.com>
2024-02-02 14:57:49 +0500

    cards updated

--- components/Offerings.js
+++ components/Offerings.js
@@ -13,7 +13,7 @@ const Offerings = () => {
                         <img src='/images/card1.webp'></img>
                     </div>
                     <div className={fcstyles.cardDetails}>
-                        <h3>Teacher Training</h3>
+                        <h3>Yoga</h3>
                         <p>Yoga classes from beginner to advanced to help you expand your life and practice.</p>
                         <button>Learn More</button>
                     </div>
@@ -22,11 +22,11 @@ const Offerings = () => {
 
                 <div className={fcstyles.card}>
                     <div className={fcstyles.cardImage}>
-                        <img src='/images/card1.webp'></img>
+                        <img src='/images/card4.webp'></img>
                     </div>
                     <div className={fcstyles.cardDetails}>
                         <h3>Teacher Training</h3>
-                        <p>Yoga classes from beginner to advanced to help you expand your life and practice.</p>
+                        <p>Take a journey into the heart of yoga and immerse yourself into a yogic lifestyle.</p>
                         <button>Learn More</button>
                     </div>
                 </div>
@@ -34,22 +34,22 @@ const Offerings = () => {
 
                 <div className={fcstyles.card}>
                     <div className={fcstyles.cardImage}>
-                        <img src='/images/card1.webp'></img>
+                        <img src='/images/card2.webp'></img>
                     </div>
                     <div className={fcstyles.cardDetails}>
-                        <h3>Teacher Training</h3>
-                        <p>Yoga classes from beginner to advanced to help you expand your life and practice.</p>
+                        <h3>Workshops & Retreats</h3>
+                        <p>Transform Your Practice and get in the flow with MĀ Yoga in Victoria, BC.</p>
                         <button>Learn More</button>
                     </div>
                 </div>
 
                 <div className={fcstyles.card}>
                     <div className={fcstyles.cardImage}>
-                        <img src='/images/card1.webp'></img>
+                        <img src='/images/card3.webp'></img>
                     </div>
                     <div className={fcstyles.cardDetails}>
-                        <h3>Teacher Training</h3>
-                        <p>Yoga classes from beginner to advanced to help you expand your life and practice.</p>
+                        <h3>Wellness Clinic</h3>
+                        <p>Ayurvedic consultations, bodywork, registered massage therapy, and Chinese medicine.</p>
                         <button>Learn More</button>
                     </div>
                 </div>
